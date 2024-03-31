ae1ca03597c61fdebe37e8831c67907dae81680a
MNuenninghoff <mikenue@gmail.com>
2024-03-31 11:55:53 -0500

    created skeleton of ParkDaoTest

---
+++ src/test/java/stateParks/persistence/ParkDaoTest.java
@@ -0,0 +1,41 @@
+package stateParks.persistence;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParkDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void findByPropertyEqual() {
+    }
+
+    @Test
+    void testFindByPropertyEqual() {
+    }
+}
